8e9d0f8f6cc3bf7ae300ec8d6ab6ba063bb81520
LINGISETTY LEELA SAI KRISHNA <129862302+Saikrishna8906@users.noreply.github.com>
2023-08-22 14:24:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var sai=new Typed(".text",{
+    strings:["Frontend Developer" , "Backend Developer" , "WebDesigner"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:100,
+    loop:true
+
+});
