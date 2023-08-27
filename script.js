32ce93937d690a232dfeba256383dba39c766a8b
LINGISETTY LEELA SAI KRISHNA <129862302+Saikrishna8906@users.noreply.github.com>
2023-08-27 13:14:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var sai=new Typed(".text",{
-    strings:["Frontend Developer" , "Backend Developer" , "WebDesigner"],
-    typeSpeed:80,
-    backSpeed:80,
-    backDelay:100,
-    loop:true
-
-});
